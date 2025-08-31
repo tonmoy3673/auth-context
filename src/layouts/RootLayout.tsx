bceccef492e3d8ef8af994f0b11fa34f21c992b9
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-08-31 19:49:20 +0600

    :truck: set routes

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/Footer";
 
 
 const RootLayout = () => {
     return (
         <div>
-            RootLayout
+           <Navbar/>
+           <Outlet/>
+           <Footer/>
         </div>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
