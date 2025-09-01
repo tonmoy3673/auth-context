7027bbee1466d56d10a3627a75e01c0c8c72c9d7
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-09-01 16:35:50 +0600

    :tada: set authContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, useState, type ReactNode } from "react";
+
+// ============ UserType ======//
+interface UserType {
+  users: {
+    name: string;
+    password: string;
+  };
+}
+
+// ============ UserTypeMethod =======//
+interface UserTypeMethod {
+  users: UserType | null;
+  login: (userData: UserType) => void;
+  logout: () => void;
+}
+
+// ============= Create Context =======//
+const AuthContext = createContext<UserTypeMethod | undefined>(undefined);
+
+// =========== AuthProvider ========//
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [users, setUser] = useState<UserType | null>(null);
+
+  // ======== login ======//
+  const login = (userData: UserType): void => {
+    if (!userData) return;
+    setUser(userData);
+  };
+
+  // =========== logout ========//
+  const logout = (): void => {
+    setUser(null);
+  };
+
+  const values = {
+    users,
+    login,
+    logout,
+  };
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
