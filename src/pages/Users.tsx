32c7c2438d8174f0e4dd773c979ac0fef65cd61b
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-09-01 16:58:22 +0600

    :pencil: set userDataTypes

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,11 +1,37 @@
+// =========== UserAddress =====//
+interface UserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+// =========== UserCompany =======//
+interface UserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+// ====== UserDataType ======//
+interface UserDataType {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: UserAddress;
+  phone: string;
+  website: string;
+  company: UserCompany;
+}
 
 const Users = () => {
-    
-    return (
-        <div>
-            
-        </div>
-    );
+
+  return <div></div>;
 };
 
 export default Users;
