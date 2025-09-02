82cbecf68b967107cead97a2689e3050730dde12
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-09-02 17:55:11 +0600

    :art: design button and card

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -8,10 +8,13 @@ const User = ({ user }: UserProps) => {
   console.log(user);
   return (
     <div className="border rounded-lg p-4 shadow bg-white">
-      <div className="">
-        <p>ID : {user?.id}</p>
-        <p>Name : {user?.name}</p>
-        <p>Email : {user?.email}</p>
+      <p>ID : {user?.id}</p>
+      <p>Name : {user?.name}</p>
+      <p>Email : {user?.email}</p>
+      <div className="mt-3 text-center">
+        <button className="text-sm bg-blue-400 text-white px-3 py-1 rounded-md cursor-pointer hover:-translate-y-1 hover:bg-amber-500 transition-all hover:scale-y-100 hover:scale-x-110 duration-150">
+          Details
+        </button>
       </div>
     </div>
   );
